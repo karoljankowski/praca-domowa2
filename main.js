b5654ebe9e39d9c216df47ef7f5c466cc08b3337
Karol Jankowski <karoljankowski@onet.eu>
2019-06-11 12:09:43 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log('siema');
+const name='Karol';
+const age=29;
+console.log(name);
+console.log(age);
+console.log(`Mam na imie ${name} i mam ${age}lat`)
+
+console.log('Witaj mordeczko ✌')
