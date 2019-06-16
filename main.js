f0fe3c83592f5d254d40a55dba8bf096bd528f71
Karol Jankowski <karoljankowski@onet.eu>
2019-06-16 19:36:53 +0200

    feat next change

--- main.js
+++ main.js
@@ -7,5 +7,10 @@ console.log(`Mam na imie ${name} i mam ${age}lat`)
 
 console.log('Witaj mordeczko ✌')
 
-const heading = document.querySelector('.classHeader');
-console.log(heading);
+const heading = document.querySelector('.about__header--js');
+console.log(heading.innerHTML);
+
+
+const paragraphs = document.querySelectorAll('p');
+console.log(paragraphs);
+console.log(paragraphs[2]);
